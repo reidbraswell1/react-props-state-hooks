0c5a03c5b4642c737ab01f53201cd81de794d0e8
reid <reidbras@gmail.com>
2022-09-11 11:54:30 -0500

    Adding logging and removing this keyword

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,8 +3,11 @@ import { useState } from "react";
 
 // Functional Component Method
 function List(props)  {
+
+    console.log(`---Begin function List()---`);
     // Destructuring
-    const {id, text} = this.props;
+    const {id, text} = props;
+
     return (<li className="list-group-item" id={id}>{text}</li>)   
 }
 export default List;
