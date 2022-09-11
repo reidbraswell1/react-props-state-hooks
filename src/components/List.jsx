03df31ef884d3899e4526a6fd106d09b12750708
reid <reidbras@gmail.com>
2022-09-11 11:30:00 -0500

    Adding function keyword

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState } from "react";
 
 // Functional Component Method
-List(props)  {
+function List(props)  {
     // Destructuring
     const {id, text} = this.props;
     return (<li className="list-group-item" id={id}>{text}</li>)   
