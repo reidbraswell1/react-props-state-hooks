5b38a48d94546a10760bcf7ac4b9e1c7371b99c9
reid <reidbras@gmail.com>
2022-09-12 11:15:36 -0500

    Updating list to iterate through the list map instead of iterating in the App

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import { useState } from "react";
 
 // Functional Component Method
 function List(props)  {
 
     console.log(`---Begin function List()---`);
     // Destructuring
-    const {id, text} = props;
+    const { list } = props;
 
-    return (<li className="list-group-item" id={id}>{text}</li>)   
+    return (<ul className="list-group mt-2 mb-2">
+                { list.map((value,index, array) => {
+                    let listId = `List:${index}`;
+                    let listText = `(${index+1}.) ${value}`;
+                       return (<li className="list-group-item" key={listId} id={listId}>{listText}</li>)
+                })}
+            </ul>)   
 }
 export default List;
